3f388a2d509f6c586868b2ab17d15afd70eab2ea
vudinhquang <vudinhquangk53@gmail.com>
2019-03-19 23:36:16 +0900

    co ban ve Routing

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const configs = require('./mmodule/config');
 
 function onRequest(req, res){
-    fs.readFile('./home.html', (err, data) => {
-        if (err) {
-            // res.writeHead(404);
-            res.write('File note found!');
-        }else{
-            res.write(data);
-        }
-        res.end();
-    });
+    const path = url.parse(req.url).pathname;
+    console.log(path);
+
+    if (path == '/about') {
+        fs.readFile('views/about.html', (err, data) => {
+            if (err) {
+                // res.writeHead(404);
+                res.write('File note found!');
+            }else{
+                res.write(data);
+            }
+            res.end();
+        });
+    } else if(path == '/'){
+        fs.readFile('views/home.html', (err, data) => {
+            if (err) {
+                // res.writeHead(404);
+                res.write('File note found!');
+            }else{
+                res.write(data);
+            }
+            res.end();
+        });
+    }
 }
 
 http.createServer(onRequest).listen(configs.port);
