0c6f9cef00ca3e4d89606a2e637842f37f950c81
vudinhquang <vudinhquangk53@gmail.com>
2019-03-21 19:28:52 +0900

    nhung giao dien cho phan add items va cai dat package express-ejs-layouts

--- project-nodejs/app.js
+++ project-nodejs/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var expressLayouts = require('express-ejs-layouts');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
 
 app.use(logger('dev'));
 app.use(express.json());
