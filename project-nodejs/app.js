3972663b18e936757de24da1acbba48f2b254c24
vudinhquang <vudinhquangk53@gmail.com>
2019-03-24 17:13:48 +0900

    ket noi voi mongoDB thong qua dich vu cua mLab

--- project-nodejs/app.js
+++ project-nodejs/app.js
@@ -5,8 +5,21 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var expressLayouts = require('express-ejs-layouts');
+var mongoose = require('mongoose');
 
 const systemConfig = require('./configs/system');
+
+
+mongoose.connect('mongodb+srv://vuquang:Vudinhquang2202@cluster0-jhlkz.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+var db = mongoose.connection;
+// db.on('error', console.log('connection error:'));
+db.on('error', () => {
+	console.log('connection error');
+});
+db.once('open', () => {
+	console.log('connected!')
+});
+
 
 var app = express();
 
@@ -31,19 +44,19 @@ app.use(`/${systemConfig.prefixAdmin}`, require('./routes/backend/index'));
 app.use('/', require('./routes/frontend/index'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('pages/error', { pageTitle: 'Page Not Found' });
+	// render the error page
+	res.status(err.status || 500);
+	res.render('pages/error', { pageTitle: 'Page Not Found' });
 });
 
 module.exports = app;
