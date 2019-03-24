af5f868ba3b6dd7f5840bbb90206f7d7b8f4386d
vudinhquang <vudinhquangk53@gmail.com>
2019-03-25 01:04:40 +0900

    Thuc hien hien thi cac nut Status Filter:All, Active, Inactive

--- project-nodejs/routes/backend/items.js
+++ project-nodejs/routes/backend/items.js
@@ -4,10 +4,16 @@ var router = express.Router();
 const ItemsModel = require('../../schemas/items');
 
 router.get('/', (req, res, next) => {
+	let statusFilter = [
+		{name: 'All', count: 1, link: '#', class: 'default'},
+		{name: 'Active', count: 2, link: '#', class: 'success'},
+		{name: 'Inactive', count: 3, link: '#', class: 'default'}
+	];
 	ItemsModel.find({}) .then((items) => {
 		res.render('pages/items/list', { 
 			pageTitle: 'Item List Page',
-			items
+			items,
+			statusFilter
 		});
 	});
 });
