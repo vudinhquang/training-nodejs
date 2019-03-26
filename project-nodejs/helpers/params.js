8dbf14ad62a9fb53dfe7a7534977508198ca969b
vudinhquang <vudinhquangk53@gmail.com>
2019-03-27 00:59:37 +0900

    viet helper getParam de lay gia tri status, gia tri mac dinh la all

---
+++ project-nodejs/helpers/params.js
@@ -0,0 +1,10 @@
+let getParam = (params, property, defaultValue) => {
+	if (params.hasOwnProperty(property) && params[property] !== undefined) {
+		return params[property];
+	}
+	return defaultValue;
+}
+
+module.exports = {
+    getParam
+}
--- project-nodejs/routes/backend/items.js
+++ project-nodejs/routes/backend/items.js
@@ -3,14 +3,17 @@ var router = express.Router();
 
 const ItemsModel = require('../../schemas/items');
 const UtilsHelpers = require('../../helpers/utils');
+const ParamsHelpers = require('../../helpers/params');
 
 // List items
 router.get('(/:status)?', (req, res, next) => {
 	let objWhere = {};
-	let currentStatus = req.params.status;
+	let currentStatus = ParamsHelpers.getParam(req.params, 'status', 'all');
+
+
 	let statusFilter = UtilsHelpers.createFilterStatus(currentStatus);
 
-	if (currentStatus !== 'all') objWhere = {status: currentStatus, ordering: 100};
+	if (currentStatus !== 'all') objWhere = {status: currentStatus};
 
 	ItemsModel.find(objWhere).then((items) => {
 		res.render('pages/items/list', { 
