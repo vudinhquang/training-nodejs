630811d00538dad5207b57617ed6da2a80a110a0
vudinhquang <vudinhquangk53@gmail.com>
2019-03-30 18:37:04 +0900

    Xay dung 2 nut Next va End

--- project-nodejs/routes/backend/items.js
+++ project-nodejs/routes/backend/items.js
@@ -7,49 +7,48 @@ const ParamsHelpers = require('../../helpers/params');
 
 // List items
 router.get('(/:status)?', (req, res, next) => {
-	let objWhere 	  = {};
-	let keyword		  = ParamsHelpers.getParam(req.query, 'keyword', '');
+	let objWhere = {};
+	let keyword = ParamsHelpers.getParam(req.query, 'keyword', '');
 	let currentStatus = ParamsHelpers.getParam(req.params, 'status', 'all');
-	let statusFilter  = UtilsHelpers.createFilterStatus(currentStatus);
-	let pagination	  = {
-		totalItems		 : 1,
+	let statusFilter = UtilsHelpers.createFilterStatus(currentStatus);
+	let pagination = {
+		totalItems: 1,
 		totalItemsPerPage: 3,
-		currentPage		 : 1,
+		currentPage: 1,
 	};
 
 	pagination.currentPage = parseInt(ParamsHelpers.getParam(req.query, 'page', '1'));
 
 	if (currentStatus !== 'all') {
 		if (keyword == '') {
-			objWhere = {status: currentStatus};
+			objWhere = { status: currentStatus };
 		} else {
-			objWhere = {status: currentStatus, name: new RegExp(keyword, 'i')};
+			objWhere = { status: currentStatus, name: new RegExp(keyword, 'i') };
 		}
 	} else {
 		if (keyword !== '') {
-			objWhere = {name: new RegExp(keyword, 'i')};
+			objWhere = { name: new RegExp(keyword, 'i') };
 		}
 	}
 
-	ItemsModel.estimatedDocumentCount({objWhere}).then((data) => {
+	ItemsModel.estimatedDocumentCount({ objWhere }).then((data) => {
 		pagination.totalItems = data;
+		ItemsModel
+			.find(objWhere)
+			.sort({ ordering: 'asc' })
+			.skip((pagination.currentPage - 1) * pagination.totalItemsPerPage)
+			.limit(pagination.totalItemsPerPage)
+			.then((items) => {
+				res.render('pages/items/list', {
+					pageTitle: 'Item List Page',
+					items,
+					statusFilter,
+					pagination,
+					currentStatus,
+					keyword
+				});
+			});
 	})
-
-	ItemsModel
-		.find(objWhere)
-		.sort({ordering: 'asc'})
-		.skip((pagination.currentPage -1)*pagination.totalItemsPerPage)
-		.limit(pagination.totalItemsPerPage)
-		.then((items) => {
-		res.render('pages/items/list', { 
-			pageTitle: 'Item List Page',
-			items,
-			statusFilter,
-			pagination,
-			currentStatus,
-			keyword
-		});
-	});
 });
 
 router.get('/add', (req, res, next) => {
