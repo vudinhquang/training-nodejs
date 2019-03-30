4bb1c7424e405f4b7230d4c12978b1cb2910f54e
vudinhquang <vudinhquangk53@gmail.com>
2019-03-30 17:20:52 +0900

    thong ke tong so trang, tong so phan tu tren 1 trang, from - to

--- project-nodejs/routes/backend/items.js
+++ project-nodejs/routes/backend/items.js
@@ -13,7 +13,7 @@ router.get('(/:status)?', (req, res, next) => {
 	let statusFilter  = UtilsHelpers.createFilterStatus(currentStatus);
 	let pagination	  = {
 		totalItems		 : 1,
-		totalItemsPerPage: 2,
+		totalItemsPerPage: 3,
 		currentPage		 : 1,
 	};
 
