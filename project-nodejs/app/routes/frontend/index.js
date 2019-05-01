70111a772f0c416631cc6eb22ebd4c4a2392c549
vudinhquang <vudinhquangk53@gmail.com>
2019-05-01 13:41:08 +0900

    Khai bao router va hien thi cac view cho category, post. Hien thi top_post len cho trang home

--- project-nodejs/app/routes/frontend/index.js
+++ project-nodejs/app/routes/frontend/index.js
@@ -2,5 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/', require('./home'));
+router.use('/category', require('./category'));
+router.use('/post', require('./post'));
 
 module.exports = router;
--- project-nodejs/app/routes/frontend/post.js
+++ project-nodejs/app/routes/frontend/post.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const folderView = __path_views_blog + '/pages/home';
+const folderView = __path_views_blog + '/pages/post';
 const layoutBlog = __path_views_blog + '/frontend';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render(folderView + '/index', {
 		layout: layoutBlog,
-		top_post: true
+		top_post: false
 	});
 });
 
