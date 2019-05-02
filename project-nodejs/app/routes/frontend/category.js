53146590652cb5a197da54975bf85c3c22546932
vudinhquang <vudinhquangk53@gmail.com>
2019-05-02 16:29:22 +0900

    Gan link cho cac category nam trong header

--- project-nodejs/app/routes/frontend/category.js
+++ project-nodejs/app/routes/frontend/category.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const ArticleModel 	= require(__path_models + '/article');
+const CategoryModel = require(__path_models + '/categories');
+
 const folderView = __path_views_blog + '/pages/category';
 const layoutBlog = __path_views_blog + '/frontend';
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/:id', async (req, res, next) => {
+	let itemsCategory = [];
+
+	await CategoryModel.listItemsFrontend({}, {'task': 'items-in-menu'}).then((items) => { 
+		itemsCategory = items; 
+	});
+
 	res.render(folderView + '/index', {
 		layout: layoutBlog,
-		top_post: false
+		top_post: false,
+		itemsCategory
 	});
 });
 
