93e1312e248ff887f7e8470c65b212c57f2c6256
vudinhquang <vudinhquangk53@gmail.com>
2020-07-14 00:24:49 +0900

    Disconnect

--- project-v3-prepare/app.js
+++ project-v3-prepare/app.js
@@ -7,7 +7,10 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
