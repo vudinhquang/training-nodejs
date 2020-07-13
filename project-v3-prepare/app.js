907153b2541ef963951f86a5adda690302405f49
vudinhquang <vudinhquangk53@gmail.com>
2020-07-14 00:01:11 +0900

    Connection

--- project-v3-prepare/app.js
+++ project-v3-prepare/app.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
