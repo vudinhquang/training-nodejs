9cb876a654220183033691b6cf2d9938deb02a18
vudinhquang <vudinhquangk53@gmail.com>
2020-07-19 00:55:47 +0900

    Show message 01

--- project-nodejs/app/routes/chat/home.js
+++ project-nodejs/app/routes/chat/home.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var moment = require('moment');
+
 const ChatsModel	 = require(__path_models + '/chats');
 const folderView	 = __path_views_chat + '/pages/home';
 const layoutChat	 = __path_views_chat + '/main';
+const systemConfig 	= require(__path_configs + '/system');
 
 module.exports = function(io) {
 	/* GET home page. */
@@ -23,7 +26,12 @@ module.exports = function(io) {
 
         socket.on('CLIENT_SEND_ALL_MESSAGE', async (data) => {
             await ChatsModel.saveItem(data, {task: "add"}).then((result) => {
-                io.emit('SERVER_RETURN_ALL_MESSAGE', result);
+                io.emit('SERVER_RETURN_ALL_MESSAGE', {
+                    content: result.content,
+                    username: result.username,
+                    avatar: result.avatar,
+                    created: moment(result.created).format(systemConfig.format_time_chat)
+                });
             });
         });
     });
