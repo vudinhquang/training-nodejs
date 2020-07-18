92b93ca2140ae834de07164a9270356849699e17
vudinhquang <vudinhquangk53@gmail.com>
2020-07-18 22:32:31 +0900

    Client send

--- project-nodejs/app/routes/chat/home.js
+++ project-nodejs/app/routes/chat/home.js
@@ -4,11 +4,22 @@ var router = express.Router();
 const folderView	 = __path_views_chat + '/pages/home/';
 const layoutChat	 = __path_views_chat + '/main';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render(`${folderView}index`, {
-		layout: layoutChat
+module.exports = function(io) {
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+		res.render(`${folderView}index`, {
+			layout: layoutChat
+		});
 	});
-});
 
-module.exports = router;
+    // socket.io events
+    io.on( "connection", function( socket ) {
+        socket.emit('SERVER_SEND_SOCKETID', socket.id);
+
+        socket.on('CLIENT_SEND_ALL_MESSAGE', (data) => {
+            console.log(data);
+        });
+    });
+    
+    return router;
+}
--- project-nodejs/app/routes/chat/index.js
+++ project-nodejs/app/routes/chat/index.js
@@ -6,12 +6,7 @@ const middleAuthenticationChat  = require(__path_middleware + '/auth-chat');
 
 module.exports = function(io) {
     router.use('/auth', require('./auth'));
-    router.use('/', middleAuthenticationChat, middleGetUserInfo, require('./home'));
-
-    // socket.io events
-    io.on( "connection", function( socket ) {
-        socket.emit('SERVER_SEND_SOCKETID', socket.id);
-        console.log( "A user connected" );
-    });
+    router.use('/', middleAuthenticationChat, middleGetUserInfo, require('./home')(io));
+    
     return router;
 }
