20157bfba233afb2416f926187a02a798e0b5af9
vudinhquang <vudinhquangk53@gmail.com>
2020-07-18 15:08:50 +0900

    Hoàn chỉnh chức năng login logout: Embed template

--- project-nodejs/app/routes/chat/home.js
+++ project-nodejs/app/routes/chat/home.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const folderView	 = __path_views_chat + '/pages/home/';
-const layoutChat	 = __path_views_chat + '/frontend/';
+const layoutChat	 = __path_views_chat + '/main';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.send('Hello Chat');
+	res.render(`${folderView}index`, {
+		layout: layoutChat
+	});
 });
 
 module.exports = router;
