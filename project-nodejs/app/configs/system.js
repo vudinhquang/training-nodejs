3d50453ae051ca2287626d8c5f676e581833844f
vudinhquang <vudinhquangk53@gmail.com>
2020-07-26 18:30:31 +0900

    Fix

--- project-nodejs/app/configs/system.js
+++ project-nodejs/app/configs/system.js
@@ -2,7 +2,7 @@ module.exports = {
     prefixAdmin: 'admin'
     , prefixBlog: 'blog'
     , prefixChat: ''
-    , env: 'dev' // Truyền production dev
+    , env: 'production' // Truyền production dev
     , format_long_time: 'DD-MM-YYYY'
     , format_time_frontend: 'DD-MM-YYYY'
     , format_time_chat: 'HH:mm DD-MM-YYYY'
